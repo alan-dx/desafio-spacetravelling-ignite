6fc43e0e98ceab07422fea48a4356ce71b299120
Alan Almeida <alanalmeida.emp@gmail.com>
2021-03-30 02:49:45 -0300

    finshed home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head'
+import Link from 'next/link'
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+import { format } from 'date-fns'
+
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser} from 'react-icons/fi'
+import { ptBR } from 'date-fns/locale';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -24,18 +33,112 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({postsPagination} : HomeProps) {
+
+  const [postsList, setPostsList] = useState(postsPagination)
+
+  async function handleLoadNextPage() {
+    
+    fetch(postsPagination.next_page)
+    .then(res => res.json())
+    .then(data => {
+      const newResults = data.results.map(post => {
+        return {
+          uid: post.uid,  
+          first_publication_date: format(new Date(post.first_publication_date), 'dd/MM/yyyy', { locale: ptBR }),
+          data: {
+            title: post.data.title,
+            subtitle: post.data.subtitle,
+            author: post.data.author
+          }
+        }
+      })
+
+      const newNextPage = data.next_page
+
+      setPostsList({
+        results: postsList.results.concat(newResults),
+        next_page: newNextPage
+      })
+
+    })
+    
+  }
+
   return (
     <>
-      <h1>Home</h1>
-      <h2>spacetravveling</h2>
+      <Head>
+        <title>Home | spacetravelling</title>
+      </Head>
+      <main className={commonStyles.container}>
+        <section className={styles.contentContainer} >
+          <Link href="/">
+            <img src="/images/logo.svg" alt="logo"/>
+          </Link>
+          <main>
+            {
+              postsList.results.map(post => (
+                <Link href={`/post/${post.uid}`}>
+                  <a key={post.uid} >
+                    <strong>{post.data.title}</strong>
+                    <p>{post.data.subtitle}</p>
+                    <div className={commonStyles.postInfo}>
+                      <FiCalendar />
+                      <span>{post.first_publication_date}</span>
+                      <FiUser />
+                      <span>{post.data.author}</span>
+                    </div>
+                  </a>
+                </Link>
+              ))
+            }
+          </main>
+          {
+            postsList.next_page && (
+              <button
+                onClick={handleLoadNextPage}
+              >
+                Carregar mais posts
+              </button>
+            )
+          }
+        </section>
+      </main>
     </>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'p1')
+  ], {
+    fetch: ['p1.title', 'p1.subtitle', 'p1.content', 'p1.author', 'p1.banner'],
+    pageSize: 2
+  });
+
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,  
+      first_publication_date: format(new Date(post.first_publication_date), 'dd/MM/yyyy', { locale: ptBR }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author
+      }
+    }
+  })
+
+  const next_page = postsResponse.next_page
+
+  return {
+    props: {
+      postsPagination : {
+        next_page,
+        results
+      }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -3,7 +3,7 @@ import { DefaultClient } from '@prismicio/client/types/client';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
   const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-    req,
+    req
   });
 
   return prismic;
